e6352775d603d23c05cb8f2401a3cf1cc8629ea9
nurzhqn0 <nurzhqn@gmail.com>
2024-11-11 06:29:49 +0500

    starter

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define rep(i,a,b) for(ll i=a;i<b;i++)
+#define per(i,b,a) for(ll i=b;i>=a;i--)
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    int x;
+    cin >> x;
+    cout << x << endl;
+    cout << x + 1;
+
+    return 0;
+}
