f5b81a2683ed49c1fec8b928562649ad69591489
nurzhqn0 <nurzhqn@gmail.com>
2024-11-11 06:41:11 +0500

    changes

--- main.cpp
+++ main.cpp
@@ -1,23 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define Kazakhstan() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define all(v) (v).begin(), (v).end()
+#define pb push_back
+#define fi first
+#define se second
 #define ll long long
 #define rep(i,a,b) for(ll i=a;i<b;i++)
 #define per(i,b,a) for(ll i=b;i>=a;i--)
 
+void solve() {
+}
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
+    Kazakhstan();
 
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 
-    int x;
-    cin >> x;
-    cout << x << endl;
-    cout << x + 1;
-
+    int t;
+    cin >> t;
+    while (t--) {
+        cout << "Hello, World!" << endl;
+    }
     return 0;
 }
